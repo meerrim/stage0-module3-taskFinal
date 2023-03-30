de051fb8c25e73a8bbcebf2b125b316a350d61da
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-03-31 00:52:22 +0600

    Completed all final tasks.

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a = number % 10;
+        int b = (number / 10) % 10;
+        int c = (number / 100) % 10;
+        int d = number / 1000;
+        int sum = a + b + c + d;
+
+        System.out.println(sum);
     }
+
 }
